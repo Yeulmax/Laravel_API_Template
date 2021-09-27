263d6aa33a80ad7916d90100549f3290390ae13a
Yeulmax <pierre.mayeul@hotmail.com>
2021-09-27 20:37:56 +0200

    Add PostController ans API routes

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// TODO Factoriser l'appel au contrôleur, problème lors de l'appel de 'route:list'
+Route::apiResource('post', 'App\Http\Controllers\PostController');
