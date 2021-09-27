20b0080cb90f14866cae1e22ca86827bab969d0e
Yeulmax <pierre.mayeul@hotmail.com>
2021-09-27 22:58:21 +0200

    Create CRUD 'post'

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,62 +3,76 @@
 namespace App\Http\Controllers;
 
 use App\Models\Post;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class PostController extends Controller
 {
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return Response
      */
     public function index()
     {
-        //
+        return Post::orderByDesc('created_at')->get();
     }
 
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * @param Request $request
+     * @return JsonResponse
      */
     public function store(Request $request)
     {
-        //
+        if(Post::create($request->all())){
+            return response()->json([
+                'success' => 'Post créée avec succès'
+            ], 200);
+        }
+
+        //TODO Ajout gestion erreur
     }
 
     /**
      * Display the specified resource.
      *
      * @param  \App\Models\Post  $post
-     * @return \Illuminate\Http\Response
+     * @return Response
      */
     public function show(Post $post)
     {
-        //
+        return $post;
     }
 
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param Request $request
      * @param  \App\Models\Post  $post
-     * @return \Illuminate\Http\Response
+     * @return JsonResponse
      */
     public function update(Request $request, Post $post)
     {
-        //
+        if($post->update($request->all())){
+            return response()->json([
+                'success' => 'Post mis à jour avec succès'
+            ], 200);
+        }
+        //TODO Ajout gestion erreur
+
     }
 
     /**
      * Remove the specified resource from storage.
      *
      * @param  \App\Models\Post  $post
-     * @return \Illuminate\Http\Response
+     * @return Response
      */
     public function destroy(Post $post)
     {
-        //
+        if($post->delete()) return response('Post supprimé', 200);
     }
 }
